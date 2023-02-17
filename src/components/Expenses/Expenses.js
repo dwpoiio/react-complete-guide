0e7b53eb4fa836a0ecabb895ce52ec2efb2790b5
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2023-02-17 09:02:06 -0300

    Unit - 65

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,12 +19,13 @@ const Expenses = (props) => {
                 onChangeFilter={filterChangeHandler} />
             {props.items.map((expense) => (
                 <ExpenseItem
+                    key={expense.id}
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}
                 />
             ))}
-            
+
         </Card>
     );
 }
