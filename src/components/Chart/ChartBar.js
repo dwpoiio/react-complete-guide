fb60031d463abadbec8c9a045fb1a34565901296
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2023-02-20 09:54:05 -0300

    Unit - 69

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,7 +3,21 @@
 import './ChartBar.css'
 
 const ChartBar = props => {
+    let barFillHeight = '0%';
 
+    if (props.mac > 0) {
+        barFillHeight = Math.round((props.value / props.max) * 100) + '%';
+    }
+
+    return <div className='chart-bar'>
+        <div className='chart-bar__inner'>
+            <div
+                className='chart-bar__fill'
+                style={{ height: barFillHeight }}
+            ></div>
+        </div>
+        <div className='chart-bar__label'>{props.label}</div>
+    </div>
 };
 
 export default ChartBar;
