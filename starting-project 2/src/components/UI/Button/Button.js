04fbf1f2ea24cb8cc1a31133c0d0dd7aa1286979
Guillermo Larrañaga <guillermo.larragodoy@gmail.com>
2023-02-21 09:29:21 -0300

    Unit - 78

--- starting-project 2/src/components/UI/Button/Button.js
+++ starting-project 2/src/components/UI/Button/Button.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
-//import './Button.css';
-
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,25 +10,20 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+  @media (min-width: 768px){
+    width: auto;
+  }
 
-&:focus {
-  outline: none;
-}
+  &:focus {
+    outline: none;
+  }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-`;
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+  `;
 
 export default Button;
